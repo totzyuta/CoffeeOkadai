e30dcfb18f4998c008c00db7b1bb41f57169529d
totzYuta <zfbfmtmxaz@gmail.com>
2014-09-28 20:11:35 +0900

    Sytem to create an event and show all events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,28 @@
+class EventsController < ApplicationController
+  before_action :authenticate
+
+  def new
+    @event = current_user.created_events.build
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event, notice: 'Event Created'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @event = Event.all
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(
+      :name, :place, :content, :start_time, :end_time
+    )
+  end
+end
